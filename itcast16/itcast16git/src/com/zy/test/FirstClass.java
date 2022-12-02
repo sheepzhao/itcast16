1ec479b820bcf8abfacdf7e1d6600197cfb94562
zhaoyang <zhaoyang@yonyou.com>
2022-12-02 16:11:44 +0800

    初次提交

---
+++ itcast16/itcast16git/src/com/zy/test/FirstClass.java
@@ -0,0 +1,13 @@
+package com.zy.test;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git world");
+        System.out.println("hello git world");
+        System.out.println("hello git world");
+        System.out.println("hello git world");
+        System.out.println("hello git world");
+        System.out.println("hello git world");
+        System.out.println("hello git world");
+    }
+}
